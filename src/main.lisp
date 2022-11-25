484c473ca5e637a21ce9c91f07fdca1182ccf9c6
ConeRobo <skihatduckie@gmail.com>
2022-11-24 20:55:03 -0500

    Create a basic window

--- src/main.lisp
+++ src/main.lisp
@@ -1,8 +1,9 @@
 ;;;; main.lisp. The entrypoint is located here.
+;;;; TEMP: Run using `sbcl --load main.lisp`
 
-;;; Dependencies
 (asdf:load-system :cffi)
 
+;; libffi is required for loading sketch
 (cffi:define-foreign-library libffi
   (t (:default "libffi-7")))
 
@@ -10,7 +11,10 @@
 
 (ql:quickload :sketch)
 
-;;; Code
-(defun main ())
+(sketch:defsketch run-app ()
+  (sketch:rect 100 100 200 200))
+
+(defun main ()
+  (make-instance `run-app))
 
 (main)
